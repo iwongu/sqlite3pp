667945d9ee0c85902f70f42f8c10c6a83a673377
Wongoo Lee <iwongu@gmail.com>
2015-02-04 14:38:34 -0800

    Move Apply functions in unnamed namespace.

--- src/sqlite3ppext.h
+++ src/sqlite3ppext.h
@@ -36,41 +36,6 @@
 namespace sqlite3pp
 {
 
-  template<size_t N>
-  struct Apply {
-    template<typename F, typename T, typename... A>
-    static inline auto apply(F&& f, T&& t, A&&... a)
-      -> decltype(Apply<N-1>::apply(std::forward<F>(f),
-                                    std::forward<T>(t),
-                                    std::get<N-1>(std::forward<T>(t)),
-                                    std::forward<A>(a)...))
-    {
-      return Apply<N-1>::apply(std::forward<F>(f),
-                               std::forward<T>(t),
-                               std::get<N-1>(std::forward<T>(t)),
-                               std::forward<A>(a)...);
-    }
-  };
-
-  template<>
-  struct Apply<0> {
-    template<typename F, typename T, typename... A>
-    static inline auto apply(F&& f, T&&, A&&... a)
-      -> decltype(std::forward<F>(f)(std::forward<A>(a)...))
-    {
-      return std::forward<F>(f)(std::forward<A>(a)...);
-    }
-  };
-
-  template<typename F, typename T>
-  inline auto apply(F&& f, T&& t)
-    -> decltype(Apply<std::tuple_size<typename std::decay<T>::type>::value>::apply(std::forward<F>(f), std::forward<T>(t)))
-  {
-    return Apply<std::tuple_size<typename std::decay<T>::type>::value>::apply(
-      std::forward<F>(f), std::forward<T>(t));
-  }
-
-
   namespace ext
   {
     template<class F>
@@ -154,6 +119,40 @@ namespace sqlite3pp
 
     namespace
     {
+      template<size_t N>
+      struct Apply {
+        template<typename F, typename T, typename... A>
+        static inline auto apply(F&& f, T&& t, A&&... a)
+          -> decltype(Apply<N-1>::apply(std::forward<F>(f),
+                                        std::forward<T>(t),
+                                        std::get<N-1>(std::forward<T>(t)),
+                                        std::forward<A>(a)...))
+        {
+          return Apply<N-1>::apply(std::forward<F>(f),
+                                   std::forward<T>(t),
+                                   std::get<N-1>(std::forward<T>(t)),
+                                   std::forward<A>(a)...);
+        }
+      };
+
+      template<>
+      struct Apply<0> {
+        template<typename F, typename T, typename... A>
+        static inline auto apply(F&& f, T&&, A&&... a)
+          -> decltype(std::forward<F>(f)(std::forward<A>(a)...))
+        {
+          return std::forward<F>(f)(std::forward<A>(a)...);
+        }
+      };
+
+      template<typename F, typename T>
+      inline auto apply(F&& f, T&& t)
+        -> decltype(Apply<std::tuple_size<typename std::decay<T>::type>::value>::apply(std::forward<F>(f), std::forward<T>(t)))
+      {
+        return Apply<std::tuple_size<typename std::decay<T>::type>::value>::apply(
+            std::forward<F>(f), std::forward<T>(t));
+      }
+
       template <class R, class... Ps>
       void functionx_impl(sqlite3_context* ctx, int nargs, sqlite3_value** values)
       {
